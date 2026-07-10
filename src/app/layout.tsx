b9ff82c5ca8ad3379046ce811bc56af7ac70c4c6
Myles Ashitey <ashim238@newschool.edu>
2026-07-10 01:44:07 -0400

    Prose adjustment, and Fresh Greens storytelling improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,7 +95,10 @@ export default function RootLayout({
         </LightboxProvider>
         <MobileNav />
         <ScrollRevealFallback />
-      </body>
+      {/* impeccable-live-start */}
+<script src="http://localhost:8400/live.js"></script>
+{/* impeccable-live-end */}
+</body>
     </html>
   );
 }
