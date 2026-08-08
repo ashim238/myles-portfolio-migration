09ac4ccc64e27674b85bd57506a6cb1c90f4bbf9
ashim238 <mylesashitey@gmail.com>
2026-08-08 02:07:08 -0400

    style: load FAFSA composer refinements

--- src/app/work/layout.tsx
+++ src/app/work/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import "../styles/portfolio-surfaces.css";
 import "../styles/navi-case-refinement.css";
+import "../styles/fafsa-case-refinement.css";
 
 export default function WorkLayout({ children }: { children: ReactNode }) {
   return children;
