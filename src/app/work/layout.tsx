7c15d321f98f28aeccfde871969627499113c6ac
ashim238 <mylesashitey@gmail.com>
2026-08-08 10:49:49 -0400

    style: load the UnderstandingFAFSA story pass

--- src/app/work/layout.tsx
+++ src/app/work/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/portfolio-surfaces.css";
 import "../styles/fresh-greens-case-refinement.css";
 import "../styles/navi-case-refinement.css";
 import "../styles/fafsa-case-refinement.css";
+import "../styles/fafsa-story-refinement.css";
 
 export default function WorkLayout({ children }: { children: ReactNode }) {
   return children;
